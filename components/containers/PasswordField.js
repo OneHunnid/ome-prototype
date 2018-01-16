e479a4f0b17fe4f5dd9b00d387a052d6fcfa95a8
climbrick <chris_limbrick@ascapny-0018.ascap.com>
2018-01-16 09:56:36 -0500

    Mask password fields

--- components/containers/PasswordField.js
+++ components/containers/PasswordField.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import $ from 'jquery'
 
 class PasswordField extends React.Component {
   constructor(props) {
@@ -10,10 +11,10 @@ class PasswordField extends React.Component {
         <div className="form-group__label">Password</div>
         <div className="form-group__description">This will be used to log in to your personal ASCAP Member Access account.</div>
         <div className="row">
-          <input placeholder="password" className="col col-12"/>
+          <input placeholder="password" className="col col-12" type="password"/>
         </div>
         <div className="row">
-          <input placeholder="verify password" className="col col-12"/>
+          <input placeholder="verify password" className="col col-12" type="password"/>
         </div>
       </div>
     )
