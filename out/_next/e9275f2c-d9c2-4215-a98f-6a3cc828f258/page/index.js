a509cc4d3fafd4d76462374ee0bda61e338d46a3
climbrick <chris_limbrick@ascapny-0018.ascap.com>
2018-03-07 09:52:35 -0500

    adding

--- out/_next/e9275f2c-d9c2-4215-a98f-6a3cc828f258/page/index.js
+++ out/_next/e9275f2c-d9c2-4215-a98f-6a3cc828f258/page/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([2],[],[281]);
+            var comp = module.exports=webpackJsonp([2],[],[279]);
             return { page: comp.default }
           })
         
